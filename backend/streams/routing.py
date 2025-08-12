057a43084c343fc4715f3195c9cc553f81f97c3a
Jethro Lim <jethro.elijahlim01@gmail.com>
2025-08-12 13:27:17 +0700

    wss

--- backend/streams/routing.py
+++ backend/streams/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('wss/stream/<path:stream_url>/', consumers.StreamConsumer.as_asgi()),
+    path('ws/stream/<path:stream_url>/', consumers.StreamConsumer.as_asgi()),
 ]
